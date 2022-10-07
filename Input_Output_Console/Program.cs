da25d8cd3d153d73e296178d97489fed1a0c6e43
mykhailolapshyn <mykhailolapshyn@MYKHAILOLAPBF28>
2022-10-07 19:40:31 +0200

    Добавьте файлы проекта.

---
+++ Input_Output_Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace InputTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введіть число: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Ви ввели число {a}");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Помилка введення!");
+            }
+        }
+    }
+}
---
+++ Input_Output_Form/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Input_Output_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show($"Ви ввели число {a}");
+            }
+            catch
+            {
+                MessageBox.Show("Помилка!");
+            }
+
+
+        }
+    }
+}
